e4d48dd2b81e218ec0214851c995d5a2483ca866
sujithamahendran <sujirenu2@gmail.com>
2017-06-27 15:56:12 +0530

    Create day.c

---
+++ day.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+char s[10];
+scanf("%s",s);
+if(strcmp(s,"sunday")==0)
+printf("false");
+else
+printf("true");
+//your code goes here
+return 0;
+}
